dbb0557ba4780fc1bd58d3b9aa04d41e2226baee
sadra-f <sadrafouladi@gmail.com>
2023-03-08 14:58:17 +0330

    added exception handeling to cluster_main_words function

--- clustering/KMeans_clustering.py
+++ clustering/KMeans_clustering.py
@@ -1,7 +1,7 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
-from statics.config import N_CLUSTERS
+from statics.config import *
 
 def kmeans(vectorized_sentences, n_clusters):
     return KMeans(n_clusters, n_init=80).fit(vectorized_sentences)
@@ -29,7 +29,20 @@ def cluster_main_words(tfidf_vector_list, km_labels):
         result.append([])
         #to plot the data accodingly uncomment below
         #tmp.plot(kind='bar')
-        for j in range(3):#pick the main 3 words in the list
-            result[i].append(tmp.index[j])
+        for j in range(N_REPRESENTING_PHRASES):# 3 is the number of main words in the cluster
+            try:
+                result[i].append(tmp.index[j])
+            except IndexError:
+                if j < 1:
+                    print('////')
+                    print(f'**ERROR** : no main words for cluster (in cluster {i})')
+                    print(np.where(km_labels == i)[0])
+                    print('////')
+                else:
+                    print('////')
+                    print(f'**Warning** : Only Found {j+1} main words (in cluster {j})')
+                    print(np.where(km_labels == i)[0])
+                    print('////')
+                break
     
     return result
