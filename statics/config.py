647174da36dec78170aaa58896db638e7beb7818
sadra-f <sadrafouladi@gmail.com>
2023-06-10 11:18:52 +0330

    added value for weight of cluster sentence in cluster distance calc

--- statics/config.py
+++ statics/config.py
@@ -6,6 +6,8 @@ KEYPHRASE = True
 TIME_DISTANCE_CONST = 0.05
 MTLS_DISTANCE_CONST = 0.3 #find proper value
 
+CLUSTER_DISTANCE_CONST = 0.4 
+
 DO_EXEC_LOG = True # execution log to console
 DO_DATA_LOG = True # log of data to file as executing through the algorithem
 
