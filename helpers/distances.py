3a7a989d1642bf1effcf566191d14ff407202730
sadra-f <sadrafouladi@gmail.com>
2023-06-10 09:49:50 +0330

    added cosine similarity func and used it in cosine distance

--- helpers/distances.py
+++ helpers/distances.py
@@ -14,7 +14,10 @@ def vector_length(vector:ndarray):
 
 def cosine_distance(vector1:ndarray, vector2:ndarray):
     #to get cosine similarity remove the "1 - ..." or subtract 1 from the result and get the absolute value
-    return 1 - ( dot(vector1, vector2) / ( vector_length(vector1) * vector_length(vector2) ) )
+    return 1 - cosine_similarity(vector1, vector2)
+
+def cosine_similarity(vector1:ndarray, vector2:ndarray):
+    return dot(vector1, vector2) / ( vector_length(vector1) * vector_length(vector2) )
 
 def sentence_distance(vector1:ndarray, date1:date, vector2:ndarray, date2:date):
     return MTLS_DISTANCE_CONST * normalized_date_diff(date1, date2) + (1 - MTLS_DISTANCE_CONST) * cosine_distance(vector1, vector2)
