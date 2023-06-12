c56bc0e6a06570cd8dd6c272d344b1d6339f9a76
sadra-f <sadrafouladi@gmail.com>
2023-06-12 13:23:02 +0330

    added cluster distance const and comment on it

--- statics/config.py
+++ statics/config.py
@@ -6,7 +6,7 @@ KEYPHRASE = True
 TIME_DISTANCE_CONST = 0.05
 MTLS_DISTANCE_CONST = 0.3 #find proper value
 
-CLUSTER_DISTANCE_CONST = 0.4 
+CLUSTER_DISTANCE_CONST = 0.4 #find proper value
 
 DO_EXEC_LOG = True # execution log to console
 DO_DATA_LOG = True # log of data to file as executing through the algorithem
