55860b8c9867f97e8669f106584b31ca56cf658c
sadra-f <sadrafouladi@gmail.com>
2023-07-01 10:05:22 +0330

    changed return type of kmeans

--- clustering/KMeans_clustering.py
+++ clustering/KMeans_clustering.py
@@ -1,10 +1,12 @@
 from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
+from models.ClusteredData import ClusteredData
 from statics.config import *
 
 def kmeans(vectorized_data, n_clusters):
-    return KMeans(n_clusters, n_init=80).fit(vectorized_data)
+    res =  KMeans(n_clusters, n_init=80).fit(vectorized_data)
+    return ClusteredData(res.labels_)
 
 
 
