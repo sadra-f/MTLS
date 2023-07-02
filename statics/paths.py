4d20f7c49f6981e8ba5cb4cd6705427ba31210c5
sadra-f <sadrafouladi@gmail.com>
2023-07-02 18:20:40 +0330

    made str paths a bit more private

--- statics/paths.py
+++ statics/paths.py
@@ -4,22 +4,22 @@
 
 
 
-STR_INPUT_PATH = 'test_io/input.txt'
-STR_LOG_PATH = 'log/.log'
+_STR_INPUT_PATH = 'test_io/input.txt'
+_STR_LOG_PATH = 'log/.log'
 
-STR_DATASET_PATH = 'dataset/mtl_dataset/L2/D3'
-STR_READY_HT_PATH = 'C:/Users/TOP/Desktop/project/mtl_dataset_HT/mtl_dataset/L2/D3'
+_STR_DATASET_PATH = 'dataset/mtl_dataset/L2/D3'
+_STR_READY_HT_PATH = 'C:/Users/TOP/Desktop/project/mtl_dataset_HT/mtl_dataset/L2/D3'
 
-STR_SENTENCE_BERT_VECTORS_PATH = 'log/SENTENCE_BERT_VECTORS/sb.npy'
-STR_SENT_LIST_PATH = 'log/sentences/sentences.npy'
-STR_SENT_HT_LIST_PATH = 'log/sentences/sentences_HT.npy'
+_STR_SENTENCE_BERT_VECTORS_PATH = 'log/SENTENCE_BERT_VECTORS/sb.npy'
+_STR_SENT_LIST_PATH = 'log/sentences/sentences.npy'
+_STR_SENT_HT_LIST_PATH = 'log/sentences/sentences_HT.npy'
 
-STR_CLUSTER1_DIST_PATH = 'log/dist1/dist1.npy'
-STR_CLUSTER1_SORTED_DIST_PATH = 'log/sorted_dist1/sortd_dist1.npy'
-STR_CLUSTER1_RES_PATH = 'log/cluster1_res/cluster1.npy'
+_STR_CLUSTER1_DIST_PATH = 'log/dist1/dist1.npy'
+_STR_CLUSTER1_SORTED_DIST_PATH = 'log/sorted_dist1/sortd_dist1.npy'
+_STR_CLUSTER1_RES_PATH = 'log/cluster1_res/cluster1.npy'
 
-STR_CLUSTER2_DIST_PATH = 'log/dist2/'
-STR_CLUSTER2_RES_PATH = 'log/cluster2_res/cluster2.npy'
+_STR_CLUSTER2_DIST_PATH = 'log/dist2/'
+_STR_CLUSTER2_RES_PATH = 'log/cluster2_res/cluster2.npy'
 #====================================================================================
 
 from pathlib import Path
