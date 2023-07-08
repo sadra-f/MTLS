9b4abd044744d4d45184e78decd539b2e65823cd
sadra-f <sadrafouladi@gmail.com>
2023-07-08 10:03:54 +0330

    removed unused code

--- clustering/KMeans_clustering.py
+++ clustering/KMeans_clustering.py
@@ -7,31 +7,3 @@ from statics.config import *
 def kmeans(vectorized_data, n_clusters):
     res =  KMeans(n_clusters, n_init=80).fit(vectorized_data)
     return ClusteredData(res.labels_)
-
-
-
-def cluster_main_words(tfidf_vector_list, labels):
-    result = []
-    for i in range(len(tfidf_vector_list)):
-        tmp = pd.DataFrame(tfidf_vector_list[i][0].toarray(), index=np.where(labels == i)[0], columns=tfidf_vector_list[i][1])
-        tmp = tmp.sum(axis=0).sort_values(ascending=False)
-        result.append([])
-        #to plot the data accodingly uncomment below
-        #tmp.plot(kind='bar')
-        for j in range(N_REPRESENTING_PHRASES):# 3 is the number of main words in the cluster
-            try:
-                result[i].append(tmp.index[j])
-            except IndexError:
-                if j < 1:
-                    print('////')
-                    print(f'**ERROR** : no main words for cluster (in cluster {i})')
-                    print(np.where(labels == i)[0])
-                    print('////')
-                else:
-                    print('////')
-                    print(f'**Warning** : Only Found {j+1} main words (in cluster {j})')
-                    print(np.where(labels == i)[0])
-                    print('////')
-                break
-    
-    return result
