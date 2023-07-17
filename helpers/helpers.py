2bdcd83d41e01fef06239b6d083e972d23a784a1
sadra-f <sadrafouladi@gmail.com>
2023-07-17 11:06:30 +0330

    moved two methods from run to helpers

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,6 +1,7 @@
 from keybert import KeyBERT
 from statics.stop_words import STOP_WORDS
 from statics.config import KEYPHRASE
+import re
 
 def sort_dist(dist:list):
     res = []
@@ -40,3 +41,49 @@ def check_samepath(filename1, filename2):
     if filename1[indx1.span()[0]:] == filename2[indx2.span()[0]:]:
         return True
     return False
+
+
+
+def extract_sentences(doc_list):
+    result = []
+    for i in range(len(doc_list)):
+        doc_ht = ht(doc_list[i].text, date=doc_list[i].date)
+        for j in range(len(doc_ht)):
+            try:
+                xml_tree = ET.fromstring(doc_ht[j])
+                if len(xml_tree) > 0 :
+                    for tag in xml_tree:
+                        doc_list[i].text[j].date = DP.parse(tag.attrib["value"], doc_list[i].date, DO_EXEC_LOG)
+                else:
+                    doc_list[i].text[j].date = doc_list[i].date
+            except Exception as e:
+                doc_list[i].text[j].date = doc_list[i].date
+            finally:
+                result.append(doc_list[i].text[j])
+    return result
+
+
+def new_extract_sentences(doc_list, HT_list):
+    result = []
+    for i in range(len(doc_list)):
+        for j in range(len(HT_list[i].text)):
+            try:
+                # for k in re.finditer("<[^<]+>( |$|)", HT_list[i].text[j]):
+                #     HT_list[i].text[j] = (HT_list[i].text[j])[:k.span()[1]-1] + '\n' + (HT_list[i].text[j])[k.span()[1]:]
+                
+                HT_list[i].text[j] = HT_list[i].text[j].replace('!doctype timeml system "timeml.dtd"', '!DOCTYPE TimeML SYSTEM "TimeML.dtd"')
+                HT_list[i].text[j] = HT_list[i].text[j].replace('timeml', 'TimeML')
+                HT_list[i].text[j] = HT_list[i].text[j].replace('timex3', 'TIMEX3')
+                HT_list[i].text[j] = HT_list[i].text[j].replace("\'", "")
+                HT_list[i].text[j] = HT_list[i].text[j].replace("&", "and")
+                xml_tree = ET.fromstring(HT_list[i].text[j])
+                if len(xml_tree) > 0 :
+                    for tag in xml_tree:
+                        doc_list[i].text[j].date = DP.parse(tag.attrib["value"], doc_list[i].date, DO_EXEC_LOG)
+                else:
+                    doc_list[i].text[j].date = doc_list[i].date
+            except Exception as e:
+                doc_list[i].text[j].date = doc_list[i].date
+            finally:
+                result.append(doc_list[i].text[j])
+    return result
