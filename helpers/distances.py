d436020984e1351d228e7889487f3d2e46f47334
sadra-f <sadrafouladi@gmail.com>
2023-07-19 13:25:15 +0330

    added comments

--- helpers/distances.py
+++ helpers/distances.py
@@ -5,23 +5,44 @@ from numpy import ndarray, dot
 from models.TStr import TStr
 
 def date_day_diff(date1:date, date2:date):
+    """
+        returns the absolute difference between two dates in days (int)
+    """
     return abs((date2 - date1).days)
 
 def normalized_date_diff(date1:date, date2:date):
+    """
+    returns the normalized difference between two dates for it to fit better into our equation (float)
+    """
     return 1 - pow(TIME_DISTANCE_CONST, date_day_diff(date1, date2))
 
 def vector_length(vector:ndarray):
+    """
+        retuns the mathmatical length of the vector (float)
+    """
     return np.sqrt(vector.dot(vector))
 
 def cosine_distance(vector1:ndarray, vector2:ndarray):
+    """
+        retuns the matmathmatical cosine distance of two vectors of the same number of features/dimensions (float)
+    """
     #to get cosine similarity remove the "1 - ..." or subtract 1 from the result and get the absolute value
     return 1 - cosine_similarity(vector1, vector2)
 
 def cosine_similarity(vector1:ndarray, vector2:ndarray):
+    """
+        returns the cosine similarity of two vectors (float)
+    """
     return dot(vector1, vector2) / ( vector_length(vector1) * vector_length(vector2) )
 
 def sentence_distance(vector1:ndarray, date1:date, vector2:ndarray, date2:date):
+    """
+        returns the distance between senteces considering their vectorized representation and the date they refer to (float)
+    """
     return MTLS_DISTANCE_CONST * normalized_date_diff(date1, date2) + (1 - MTLS_DISTANCE_CONST) * cosine_distance(vector1, vector2)
 
 def cluster_distance(cluster_vector_1:ndarray, sent1, cluster_vector_2:ndarray, sent2):
+    """
+        returns the distance between document vector of two clusters and the vector for the representing sentence of the cluster (float)
+    """
     return CLUSTER_DISTANCE_CONST * cosine_distance(sent1, sent2) + (1 - CLUSTER_DISTANCE_CONST) * cosine_distance(cluster_vector_1, cluster_vector_2)
