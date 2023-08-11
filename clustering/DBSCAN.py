f5918a79041004a2da6506a0b07738380a51a1c2
sadra-f <sadrafouladi@gmail.com>
2023-08-11 18:34:26 +0330

    fixed problem where we were using distances as X values instead of sitance values

--- clustering/DBSCAN.py
+++ clustering/DBSCAN.py
@@ -2,5 +2,5 @@ from sklearn.cluster._dbscan import DBSCAN
 from models.ClusteredData import ClusteredData
 
 def dbscan(inp_array, eps, min_samples):
-    clusterer = DBSCAN(eps=eps, min_samples=min_samples).fit(inp_array)
+    clusterer = DBSCAN(eps=eps, min_samples=min_samples, metric='precomputed').fit(inp_array)
     return ClusteredData(clusterer.labels_)
