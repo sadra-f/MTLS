c4f02e072e7a444da8245f47b3b49d375f79f26d
sadra-f <sadrafouladi@gmail.com>
2023-08-13 16:19:42 +0330

    added function to calculate eps for dbscan

--- clustering/helpers.py
+++ clustering/helpers.py
@@ -12,3 +12,13 @@ def cluster_inp_list(inp_sentence_list, cluster_labels, cluster_count):
             clustered_sentences[ci].append(inp_sentence_list[inpi])
     
     return clustered_sentences
+
+
+def dbscan_eps(distances, min_points):
+    """
+        distances: the distance matrix to be given to the dbscan algorithem to find clusters within
+        min_points: the minimum points to be around a point for it to be a centroid
+
+        returns: desirable epsilon value for dbscan algorithem
+    """
+    return np.sum([float(np.sort(val, axis=0)[min_points]) for val in distances]) / len(distances)
