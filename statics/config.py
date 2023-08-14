7f1849fcdd96f70a666a6551e012be60493bef8d
sadra-f <sadrafouladi@gmail.com>
2023-08-14 16:46:24 +0330

    removed the ep values because of the new  eps func

--- statics/config.py
+++ statics/config.py
@@ -11,8 +11,8 @@ CLUSTER_DISTANCE_CONST = 0.4 #find proper value
 DO_EXEC_LOG = True # execution log to console
 DO_DATA_LOG = True # log of data to file as executing through the algorithem
 
-DBSCAN_EPSILON_1 = 0.4
+# DBSCAN_EPSILON_1 = 0.4
 DBSCAN_MINPOINT_1 = 5
 
-DBSCAN_EPSILON_2 = 1.2
-DBSCAN_MINPOINT_2 = 2
+# DBSCAN_EPSILON_2 = 1.2
+DBSCAN_MINPOINT_2 = 5
