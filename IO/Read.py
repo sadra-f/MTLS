b3610737bfd38173843489d23107506e62bf9052
sadra-f <sadrafouladi@gmail.com>
2023-11-07 17:51:40 +0330

    added documentation

--- IO/Read.py
+++ IO/Read.py
@@ -2,4 +2,12 @@ import numpy as np
 
 
 def read_np_array(path):
+    """Raed a pickled/not pickled file containing the data in an numpy array
+
+    Args:
+        path (Path|str): the path in which to look for the files
+
+    Returns:
+        ndarray: the numpy array which was saved into the given file
+    """
     return np.load(path, allow_pickle=True)
