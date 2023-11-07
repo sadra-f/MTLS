7864d07323064d34069fb4c368622f5431ae8364
sadra-f <sadrafouladi@gmail.com>
2023-11-07 17:51:43 +0330

    added documentation

--- IO/Write.py
+++ IO/Write.py
@@ -3,4 +3,10 @@ import numpy as np
 
 
 def write_np_array(arr, path):
+    """Writes a numpy array into a file, use pickling if needed
+
+    Args:
+        arr (ndarray): the numpy array to be written
+        path (Path|str): the path into which ti write the array
+    """
     np.save(path, arr, allow_pickle=True)
