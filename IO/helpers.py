dc2e7e1b215acd2b04bac8634d7d80b228f92335
sadra-f <sadrafouladi@gmail.com>
2023-11-08 11:00:51 +0330

    removed read_gt func

--- IO/helpers.py
+++ IO/helpers.py
@@ -23,36 +23,3 @@ def docs_of_pattern(dir:Path, pattern="*.txt", recursive=True):
         except StopIteration:   
             break
     return (result, counter)
-
-
-def read_ground_truth(dir:Path): # returns a list of date,text tuples
-    """Reads through the ground truth files and returns their content as tuples 
-
-    Args:
-        dir (Path): the path in which to search for the files
-
-    Returns:
-        _type_: a list of tuples each containing the text and the date of the text
-    """
-    gt_text = []
-    with open(dir, 'r') as file:
-        gt_text = file.readlines()
-
-    res = []
-    date = None
-    text = ""
-    for line in gt_text :
-        if re.search("^-+$", line) is not None:
-            res.append((date, text))
-            date = None
-            text = ""
-            continue
-        elif re.search("\d{4}-\d{2}-\d{2}", line) is not None:
-            line = line.strip()
-            date = datetime.strptime(line, "%Y-%m-%d")
-            continue
-        else:
-            text += line + "\n"
-            continue
-    
-    return res
