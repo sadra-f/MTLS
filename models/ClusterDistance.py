e3ce5a4e39cc56577a729892bd3f96b5717366af
sadra-f <sadrafouladi@gmail.com>
2024-01-10 23:26:02 +0330

    deleted redundant code

--- models/ClusterDistance.py
+++
@@ -1,7 +0,0 @@
-class DistanceKmeans:
-    """Temporary class for test
-    """
-    def __init__(self,vector,examplar_sen):
-        self.vector=vector
-        self.examplar_sen=examplar_sen
-        
