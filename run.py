66f1e5678f717d09d2ae4f50a7e98eeb58b68951
sadra-f <sadrafouladi@gmail.com>
2024-07-03 15:06:40 +0330

    added extra logging and some refactoring

--- run.py
+++ run.py
@@ -41,10 +41,10 @@ def main():
     ht_doc_list = DocumentReader(READY_HT_PATH, file_pattern="*.htrs",parent_dir_as_date=True).read_all()
     sent_list = new_extract_sentences(doc_list, ht_doc_list)
     if READ_SB_FROM_LOG:
-        sb_result = read_np_array(SENTENCE_BERT_VECTORS_PATH)
+        sb_result = read_np_array(SENTENCE_BERT_VECTORS_PATH, N_TIMELINES, DATASET_NUMBER)
     else:
         sb_result = sb(sent_list)
-        write_np_array(sb_result, SENTENCE_BERT_VECTORS_PATH)
+        write_np_array(sb_result, SENTENCE_BERT_VECTORS_PATH, N_TIMELINES, DATASET_NUMBER)
     
     for i, bert in enumerate(sb_result):
         sent_list[i].id = i
@@ -68,14 +68,14 @@ def main():
                     dists[i][j][k] = dists[i][k][j] = sentence_distance(cluster[j], cluster[k])
             print('distances: ', datetime.datetime.now())
         
-        write_np_array(dists, CLUSTER1_DIST_PATH)
-        write_np_array(init_KM_clusters.labels, INIT_CLUSTER_LABELS_PATH)
-        write_np_array(init_clustered_sentences, INIT_CLUSTER_SENT_PATH)
+        write_np_array(dists, CLUSTER1_DIST_PATH, N_TIMELINES, DATASET_NUMBER)
+        write_np_array(init_KM_clusters.labels, INIT_CLUSTER_LABELS_PATH, N_TIMELINES, DATASET_NUMBER)
+        write_np_array(init_clustered_sentences, INIT_CLUSTER_SENT_PATH, N_TIMELINES, DATASET_NUMBER)
 
     else:
-        dists = read_np_array(CLUSTER1_DIST_PATH)
-        init_KM_clusters = ClusteredData(read_np_array(INIT_CLUSTER_LABELS_PATH))
-        init_clustered_sentences = read_np_array(INIT_CLUSTER_SENT_PATH)
+        dists = read_np_array(CLUSTER1_DIST_PATH, N_TIMELINES, DATASET_NUMBER)
+        init_KM_clusters = ClusteredData(read_np_array(INIT_CLUSTER_LABELS_PATH, N_TIMELINES, DATASET_NUMBER))
+        init_clustered_sentences = read_np_array(INIT_CLUSTER_SENT_PATH, N_TIMELINES, DATASET_NUMBER)
     
 
     clustered_sentences = []
@@ -85,7 +85,8 @@ def main():
         clustered_sentences.extend(cluster_inp_list(init_clustered_sentences[i], clusters.labels, clusters.cluster_count))
     
     # p2a(clustered_sentences, "C:/Users/TOP/Desktop/New folder/dbscan_sentences.txt")
-    FIRST_CLUSTER_COUNT = -1
+    FIRST_CLUSTER_COUNT = len(clustered_sentences)
+    print("Event Count : ", FIRST_CLUSTER_COUNT)
 
     #hold sentence and bert next sentence probability
     bfnsp_cluster_sentence = []
@@ -93,6 +94,9 @@ def main():
     subjs = None
     if not READ_BFNSP_FROM_LOG:
         # keybert key phrase finder
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
+        
         cluster_main_phrases = doc_list_kewords_sentence(clustered_sentences)
         subjs = mpc(cluster_main_phrases)
 
@@ -103,13 +107,11 @@ def main():
             for subj in subjs:
                 count += len(re.findall(f"{subj[1]}", " ".join(event)))
             counts.append(count)
-            if count >= 10 :
+            if count >= 13 :
                 cleaned_events.append(event)
         clustered_sentences = cleaned_events
         FIRST_CLUSTER_COUNT = len(clustered_sentences)
         # p1a(cluster_main_phrases, "C:/Users/TOP/Desktop/New folder/dbscan_cluster_main_phrases.txt")
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        model = BertForNextSentencePrediction.from_pretrained('bert-base-uncased')
         for j in range(FIRST_CLUSTER_COUNT):
             bfnsp_cluster_sentence.append([])
             for i in range(len(clustered_sentences[j])):
@@ -124,9 +126,9 @@ def main():
 
             bfnsp_cluster_sentence[j] = sorted(bfnsp_cluster_sentence[j], key=lambda x: x[1], reverse=True)
         
-        write_np_array(bfnsp_cluster_sentence, BFNSP_RES_PATH)
+        write_np_array(bfnsp_cluster_sentence, BFNSP_RES_PATH, N_TIMELINES, DATASET_NUMBER)
     else:
-        bfnsp_cluster_sentence = read_np_array(BFNSP_RES_PATH).tolist()
+        bfnsp_cluster_sentence = read_np_array(BFNSP_RES_PATH, N_TIMELINES, DATASET_NUMBER).tolist()
 
 
     #clean from the unrelevant events
@@ -136,7 +138,7 @@ def main():
     cluster_vectors = np.full((FIRST_CLUSTER_COUNT, ), None, object)
 
     for i in range(FIRST_CLUSTER_COUNT):
-        cluster_vectors[i] = DXCV(np.zeros((DOCUMENT_COUNT,)), bfnsp_cluster_sentence[i][0][0].vector, clsv(cluster_main_phrases[i], subjs))
+        cluster_vectors[i] = DXCV(np.zeros((DOCUMENT_COUNT,)), bfnsp_cluster_sentence[i][0][0].vector)
         for j in range(len(clustered_sentences[i])):
             cluster_vectors[i].doc_cluster_vector[clustered_sentences[i][j].doc_id] += 1
 
@@ -171,8 +173,13 @@ def main():
 
     print(evaluations)
     print(datetime.datetime.now())
-    with open(f'../Results/L{N_TIMELINES}D{DATASET_NUMBER}.txt', 'a') as f:
+    with open(f'../Results/L{N_TIMELINES}D{DATASET_NUMBER}.txt', 'w') as f:
         print(f'{evaluations}', file=f)
+    with open(f'../Result_timelines/L{N_TIMELINES}D{DATASET_NUMBER}.txt', 'w') as f:
+        for i in timelines_clusters_sentences:
+            for j in i:
+                print(f'{j[0]} ==> ({j[1]})', file=f)
+            print("\r\n", file=f)
     return
     
 
