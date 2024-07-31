fb94783622c00568bee9ce479b0d714c0db860bb
sadra-f <sadrafouladi@gmail.com>
2024-07-31 13:52:30 +0330

    set date for GTs

--- IO/Read.py
+++ IO/Read.py
@@ -21,7 +21,7 @@ def read_all_GTs(dataset_path:Path, N_TIMELINES):
     res = []
     for i in range(N_TIMELINES):
         gt_path = dataset_path / 'groundtruth' / f'g{i+1 if N_TIMELINES > 1 else ""}'
-        res.append(read_ground_truth(gt_path))
+        res.append(_new_read_ground_truth(gt_path))
     return res
 
 def read_ground_truth(dir:Path): # returns a list of date,text tuples
@@ -54,6 +54,39 @@ def read_ground_truth(dir:Path): # returns a list of date,text tuples
             continue
         else:
             text += line + "\n"
+            continue
+    
+    return res
+
+def _new_read_ground_truth(dir:Path):
+    """Reads through the ground truth files and returns their content as tuples 
+
+    Args:
+        dir (Path): the path in which to search for the files
+
+    Returns:
+        _type_: a list of tuples each containing the text and the date of the text
+    """
+    gt_text = []
+    with open(dir, 'r') as file:
+        gt_text = file.readlines()
+
+    res = []
+    date = None
+    text = ""
+    for line in gt_text :
+        if re.search("^-+$", line) is not None:
+            date = None
+            text = ""
+            continue
+        elif re.search("\d{4}-\d{2}-\d{2}", line) is not None:
+            line = line.strip()
+            date = datetime.strptime(line, "%Y-%m-%d")
+            continue
+        else:
+            text = TStr(line)
+            text.date = date
+            res.append(text)
             continue
     
     return res
